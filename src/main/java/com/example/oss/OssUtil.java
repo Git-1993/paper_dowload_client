cca7278bcd171a4ba4dc50e385818d84e708a310
郭彪 <670521186@qq.com>
2018-09-05 17:06:04 +0800

    整理后代码

--- src/main/java/com/example/oss/OssUtil.java
+++ src/main/java/com/example/oss/OssUtil.java
@@ -21,32 +21,6 @@ import com.aliyun.openservices.oss.model.ObjectMetadata;
 public class OssUtil {
 
     /**
-     *
-     *@ClassName: TestOSS
-     *
-     *@Description: 该示例代码展示了如果在OSS中创建和删除一个Bucket，以及如何上传和下载一个文件。
-     *
-     * 该示例代码的执行过程是：
-     * 1. 创建一个Bucket（如果已经存在，则忽略错误码）；
-     * 2. 上传一个文件到OSS；
-     * 3. 下载这个文件到本地；
-     * 4. 清理测试资源：删除Bucket及其中的所有Objects。
-     *
-     * 尝试运行这段示例代码时需要注意：
-     * 1. 为了展示在删除Bucket时除了需要删除其中的Objects,
-     *    示例代码最后为删除掉指定的Bucket，因为不要使用您的已经有资源的Bucket进行测试！
-     * 2. 请使用您的API授权密钥填充ACCESS_ID和ACCESS_KEY常量；
-     * 3. 需要准确上传用的测试文件，并修改常量uploadFilePath为测试文件的路径；
-     *    修改常量downloadFilePath为下载文件的路径。
-     * 4. 该程序仅为示例代码，仅供参考，并不能保证足够健壮。
-     *OSS Java API手册：http://aliyun_portal_storage.oss.aliyuncs.com/oss_api/oss_javahtml/index.html?spm=5176.7150518.1996836753.8.d5TjaG
-     *OSS Java SDK开发包:http://help.aliyun.com/view/13438814.html
-     *OSSClient:www.mvnrepository.com/artifact/cglib/cglib/2.2
-     *@author PineTree
-     *@date 2014年12月1日 下午3:23:32
-     *@version
-     */
-    /**
      * 阿里云ACCESS_ID
      */
     private static String ACCESS_ID = "vtXujYmNXNaTGV93";
@@ -64,65 +38,30 @@ public class OssUtil {
      */
     private static String BUCKET_NAME = "test-upload-paper";
 
-
     public static void testUpload(String Objectkey, String uploadFilePath) {
-        //String bucketName = "demo10";
-//            String Objectkey = "photo1.jpg";
-//
-//            String uploadFilePath = "D:\\photo.jpg";
-//            String downloadFilePath = "D:\\photo1.jpg";
-
         // 使用默认的OSS服务器地址创建OSSClient对象,不叫OSS_ENDPOINT代表使用杭州节点，青岛节点要加上不然包异常
         OSSClient client = new OSSClient(OSS_ENDPOINT, ACCESS_ID, ACCESS_KEY);
-
-        //如果你想配置OSSClient的一些细节的参数，可以在构造OSSClient的时候传入ClientConfiguration对象。
-        //ClientConfiguration是OSS服务的配置类，可以为客户端配置代理，最大连接数等参数。
-        //具体配置看http://aliyun_portal_storage.oss.aliyuncs.com/oss_api/oss_javahtml/OSSClient.html#id2
-        //ClientConfiguration conf = new ClientConfiguration();
-        //OSSClient client = new OSSClient(OSS_ENDPOINT, ACCESS_ID, ACCESS_KEY, conf);
-
-
         try {
             ensureBucket(client, BUCKET_NAME);
             setBucketPublicReadable(client, BUCKET_NAME);
-
             System.out.println("正在上传...");
             uploadFile(client, BUCKET_NAME, Objectkey, uploadFilePath);
-
-//            System.out.println("正在下载...");
-//            downloadFile(client, BUCKET_NAME, Objectkey, downloadFilePath);
+            System.out.println("上传完成!");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            System.out.println("上传完成!");
-            // deleteBucket(client, BUCKET_NAME);
         }
     }
 
     public static void testDowload(String Objectkey, String downloadFilePath) {
-        //String bucketName = "demo10";
-//            String Objectkey = "photo1.jpg";
-//
-//            String uploadFilePath = "D:\\photo.jpg";
-//            String downloadFilePath = "D:\\photo1.jpg";
-
         // 使用默认的OSS服务器地址创建OSSClient对象,不叫OSS_ENDPOINT代表使用杭州节点，青岛节点要加上不然包异常
         OSSClient client = new OSSClient(OSS_ENDPOINT, ACCESS_ID, ACCESS_KEY);
-
         //如果你想配置OSSClient的一些细节的参数，可以在构造OSSClient的时候传入ClientConfiguration对象。
         //ClientConfiguration是OSS服务的配置类，可以为客户端配置代理，最大连接数等参数。
         //具体配置看http://aliyun_portal_storage.oss.aliyuncs.com/oss_api/oss_javahtml/OSSClient.html#id2
         //ClientConfiguration conf = new ClientConfiguration();
         //OSSClient client = new OSSClient(OSS_ENDPOINT, ACCESS_ID, ACCESS_KEY, conf);
-
-
         try {
-//            ensureBucket(client, BUCKET_NAME);
-//            setBucketPublicReadable(client, BUCKET_NAME);
-//
-//            System.out.println("正在上传...");
-//            uploadFile(client, BUCKET_NAME, Objectkey, uploadFilePath);
-
             System.out.println("正在下载...");
             downloadFile(client, BUCKET_NAME, Objectkey, downloadFilePath);
             System.out.println("下载完成...");
@@ -182,7 +121,6 @@ public class OssUtil {
     private static void setBucketPublicReadable(OSSClient client, String bucketName) throws OSSException, ClientException {
         //创建bucket
         client.createBucket(bucketName);
-
         //设置bucket的访问权限， public-read-write权限
         client.setBucketAcl(bucketName, CannedAccessControlList.PublicRead);
     }
